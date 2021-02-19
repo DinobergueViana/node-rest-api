8d26f0f80264ec24338274e762ce152d34a1b090
DinobergueViana <dinobergueviana@gmail.com>
2021-02-19 15:34:14 -0300

    configurando servidor

---
+++ server.js
@@ -0,0 +1,8 @@
+// setting sever setup
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+server.listen(port);
